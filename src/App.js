98161434da2623dcb5618bde93384e83e389ce9b
Suraj Pradip <surajpradip090@gmail.com>
2021-06-09 19:12:36 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./Componants/Main";
+import { useEffect , useState } from "react";
+import axios from "axios";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import 'jquery/dist/jquery.min.js'
+import 'datatables.net-dt/css/jquery.dataTables.min.css'
+import 'datatables.net-dt/js/dataTables.dataTables'
+import $ from "jquery";
 
 function App() {
+
+  const [covidData,setCovidData] = useState([])
+
+  useEffect(() =>{
+    axios.get('https://api.covid19api.com/summary').then(res =>{
+      setCovidData(res.data.Countries)
+    }).catch(err => console.log(err))
+
+    $(document).ready(()=>{
+      $('#tablemine').DataTable()
+    })
+
+  },[covidData])
+
+  function Table() {
+
+  
+    return (
+        <div className>
+            <div className='row justify-content-center'>
+                <div className='col-md-8'>
+                    <table id="tablemine" className='table table-dark table-bordered'>
+                        <thead>
+                            <tr style={{color : 'yellow'}}>
+                                <th>Country</th>
+                                <th>Cofirmed</th>
+                                <th>Active Cases</th>
+                                <th>Cured</th>
+                                <th>Deaths</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                          {tableData}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+ 
+  const tableData = covidData.map(obj =>{
+     return(  <tr>
+        <td>{obj.Country}</td>
+        <td>{obj.TotalConfirmed}</td>
+        <td>{obj.TotalConfirmed-obj.TotalRecovered}</td>
+        <td>{obj.TotalRecovered}</td>
+        <td>{obj.TotalDeaths}</td>
+      </tr>)
+  })
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header>
+        <div className='overlay'>
+        <Main />
+        </div>
+        
       </header>
+      <div className='bg'>
+       {Table()}
+      </div>
+     
     </div>
   );
 }
---
+++ src/Componants/Main.js
@@ -0,0 +1,11 @@
+
+const Main = () => {
+    return (
+        <div>
+          <h1>Covid Stats</h1>  
+          <h3>Information from 190 countries</h3>
+        </div>
+    )
+}
+
+export default Main
